39855ad85169f5b2b1ef03e76b0df81765d63941
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-03-01 03:11:50 +0700

    practise01

---
+++ src/main/java/cg/service/ICategoryService.java
@@ -0,0 +1,13 @@
+package cg.service;
+
+import cg.model.Category;
+
+import java.util.Optional;
+
+public interface ICategoryService {
+
+    Iterable<Category> findAll();
+
+    Category findById(Long id);
+
+}
